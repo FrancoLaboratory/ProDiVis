fixed bit depths for normalization

scripts/norm_heatmap.py
@@ -82,7 +82,7 @@ def stack(tiff_list, viewpoints, z_mult, norm = False):
                     og_row = int(np.floor(rdx / z_mult))
                     out[rdx][cdx] = img_list[idx][og_row][cdx]
             img_list[idx] = out
-        img_list[idx] = tools.min_max_scale(img_list[idx])
+        img_list[idx] = tools.min_max_scale(img_list[idx], np.float32)
 
     return img_list
 

scripts/normalize.py
@@ -15,10 +15,20 @@ import tools
 # Post-Conditions:
 # Will load in any bitdepth as of 06/19/2024
 def get_norm_bool_idxs(norm_slice_paths, bool_cutoff):
-    img1 = cv2.cvtColor(cv2.imread(norm_slice_paths[0], -1), cv2.COLOR_BGR2GRAY)
+    img1 = cv2.imread(norm_slice_paths[0], -1)
+    if len(img1.shape) != 2:
+        if img1.shape[-1] == 3:
+            img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        else:
+            raise ValueError(f"Expected an image with 1 or 3 channels, not {img1.shape[-1]}")
     slice_bool_cumulative = np.empty(img1.shape, dtype = np.uint8)
     for slice_path in norm_slice_paths:
-        norm_slice = cv2.cvtColor(cv2.imread(slice_path, -1), cv2.COLOR_BGR2GRAY)
+        norm_slice = cv2.imread(norm_slice_paths[0], -1)
+        if len(norm_slice.shape) != 2:
+            if norm_slice.shape[-1] == 3:
+                norm_slice = cv2.cvtColor(norm_slice, cv2.COLOR_BGR2GRAY)
+            else:
+                raise ValueError(f"Expected an image with 1 or 3 channels, not {norm_slice.shape[-1]}")
         norm_slice[norm_slice > 0] = 1
         slice_bool_cumulative = slice_bool_cumulative + norm_slice
     final_cumulative = slice_bool_cumulative / len(norm_slice_paths)
@@ -60,7 +70,7 @@ def mean_normalizer(tiff_list, norm_list, threshold, outlier_stddevs, raw_norm, 
     log_10s = math.floor(math.log(len(tiff_list), 10)) + 14
 
     for tiff, norm in zip(np.sort(tiff_list), np.sort(norm_list)):
-        print(f"Normalizing {tiff_dirname} using {norm_dirname} ({count}/{len(tiff_list)})...", end = '\n')
+        print(f"\rNormalizing {tiff_dirname} using {norm_dirname} ({count}/{len(tiff_list)})...", end = '\n')
         if ign_mono:
             norm_tiffs.append(mean_normalize(tiff, norm, out_dir, threshold, outlier_stddevs, raw_norm, log_10s, norm_bool))
         else:
@@ -80,7 +90,12 @@ def mean_normalizer(tiff_list, norm_list, threshold, outlier_stddevs, raw_norm, 
 # Post-Conditions:
 # Will load in any bitdepth as of 06/19/2024
 def tiff_mean(normpath, thresh, stddevs, raw_norm, norm_bool):
-    normBW = cv2.cvtColor(cv2.imread(normpath, -1), cv2.COLOR_BGR2GRAY)
+    normBW = cv2.imread(normpath, -1)
+    if len(normBW.shape) != 2:
+        if normBW.shape[-1] == 3:
+            normBW = cv2.cvtColor(normBW, cv2.COLOR_BGR2GRAY)
+        else:
+            raise ValueError(f"Expected an image with 1 or 3 channels, not {normBW.shape[-1]}")
     normBW = normBW.astype(float)
     if type(norm_bool) != int:
         normBW[norm_bool == 0] = np.nan
@@ -98,7 +113,12 @@ def tiff_mean(normpath, thresh, stddevs, raw_norm, norm_bool):
 # Post-Conditions:
 # Will load in any bitdepth as of 06/19/2024
 def tiff_dist(normpath, lower, upper, norm_bool):
-    normBW = cv2.cvtColor(cv2.imread(normpath, -1), cv2.COLOR_BGR2GRAY)
+    normBW = cv2.imread(normpath, -1)
+    if len(normBW.shape) != 2:
+        if normBW.shape[-1] == 3:
+            normBW = cv2.cvtColor(normBW, cv2.COLOR_BGR2GRAY)
+        else:
+            raise ValueError(f"Expected an image with 1 or 3 channels, not {normBW.shape[-1]}")
     normBW = normBW.astype(float)
     if type(norm_bool) != int:
         normBW[norm_bool == 0] = np.nan
@@ -196,6 +216,7 @@ def mean_normalize(tiffpath, normpath, out_dir, thresh, stddevs, raw_norm, len_l
         raise ValueError(f"Z-value for reference tiff ({tiffZ}) is not equal to Z-value for normalization tiff ({normZ})")
 
     # Will load in any bitdepth as of 06/19/2024
+    tiffBW = cv2.imread(tiffpath, -1)
     if len(tiffBW.shape) != 2:
         if tiffBW.shape[-1] == 3:
             tiffBW = cv2.cvtColor(tiffBW, cv2.COLOR_BGR2GRAY)

scripts/tools.py
@@ -125,16 +125,19 @@ def get_keyword_files(folder, key):
 # Description: Performs min-max scaling on img object (pixel array)
 # Pre-Conditions: Img object provided
 # Post-Conditions: Return img object with pixels min-max scaled.
-def min_max_scale(img):
+def min_max_scale(img, dtype = None):
     np.seterr(all = 'raise')
 
     minimum = img.min()
     maximum = img.max()
 
+    if type(dtype) == type(None):
+        dtype = img.dtype
+
     try:
-        dtype = np.iinfo(img.dtype)
+        dtype = np.iinfo(dtype)
     except:
-        dtype = np.finfo(img.dtype)
+        dtype = np.finfo(dtype)
     dtype_max = dtype.max
     
     # The formula for min-max scaling: